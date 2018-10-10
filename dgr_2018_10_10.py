c9167ea8b42d6695c5197625edd3e432947f67ba
user <anastasija.pluhina@gmail.com>
2018-10-10 10:02:55 +0300

    2018_10_10_10_02

---
+++ dgr_2018_10_10.py
@@ -0,0 +1,28 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: /home/user/test20191010.py ====================
+equals 5
+Greater than 4
+>>> 
+==================== RESTART: /home/user/test20191010.py ====================
+equals 5
+>>> 
+==================== RESTART: /home/user/test20191010.py ====================
+equals 1.0
+>>> 
+==================== RESTART: /home/user/test20191010.py ====================
+Enter a number between 0.0 and 1.0:0.85
+
+Traceback (most recent call last):
+  File "/home/user/test20191010.py", line 2, in <module>
+    score = float(input)
+TypeError: float() argument must be a string or a number
+>>> 
+==================== RESTART: /home/user/test20191010.py ====================
+Enter a number between 0.0 and 1.0:
+==================== RESTART: /home/user/test20191010.py ====================
+Enter a number between 0.0 and 1.0:0.85
+B
+>>> 
